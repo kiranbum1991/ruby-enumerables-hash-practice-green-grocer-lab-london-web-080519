e456ed6d3aaa1359e5edbdd105ff8ea7cd5d1a39
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-03 11:49:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,8 +1,17 @@
 require 'pry'
 def consolidate_cart(cart)
   # code here
-  cart.each do |item, attributes|
-    binding.pry
+  hash = {}
+  cart.each do |item|
+    item.each do |name, price|
+      if hash[name].nil?
+        hash[name] = price.merge({:count => 1})
+      else 
+        hash[name][:count] += 1
+    end
+  end
+end
+hash
 end
 
 def apply_coupons(cart, coupons)
